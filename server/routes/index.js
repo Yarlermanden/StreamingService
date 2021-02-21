482bb2d5e05b6201457ac72e65ab1bfa3999f524
MMHO <MMHO@itu.dk>
2021-02-21 21:18:39 +0100

    minor changes

--- server/routes/index.js
+++ server/routes/index.js
@@ -6,7 +6,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) => {
-	res.send({ response: 'i am alive' }). status(20);
+	res.send({ response: 'i am alive' }). status(200);
 })
 
 module.exports = router;
--- test.py
+++ test.py
@@ -1,25 +1,35 @@
-import socket
+#import socket
+from socketIO_client import SocketIO, LoggingNamespace
 import sys
 
 host = '192.168.1.83'
 port = 5003
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address = (host, port)
+
+
+
+
+
+#sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#server_address = (host, port)
 
 #sock.connect(server_address)
-sock.connect((host, port))
+#sock.connect((host, port))
 
 try:
-    message = "this is a message"
-    for i in range(1):
-        sock.send(message)
+    socketIO = SocketIO(host, port)
+    socketIO.emit('aaa')
+    socketIO.wait(seconds=1)
+    #message = "this is a message"
+    #sock.send(message)
+    #for i in range(1):
 
         #while amount_received < amount_expected:
             #data=sock.recv(16)
             #amount_received += len(data)
-    amount_received = 0
-    amount_expected = len(message)
+
+    #amount_received = 0
+    #amount_expected = len(message)
 
 
 
@@ -27,4 +37,4 @@ except:
     print("error")
 finally:
     print('closing socket')
-    sock.close()
+    #sock.close()
